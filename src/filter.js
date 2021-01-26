c922937012db5f7d1cc1004af4abab383ed24d65
Bernard <bernard.leech@crunch.co.uk>
2021-01-26 16:25:04 +0000

    formatted files

--- src/filter.js
+++ src/filter.js
@@ -1,31 +1,30 @@
-module.exports = function Filter(string) {
-
-  string = string || '';
-  var parsed = compile(string);
+module.exports = function Filter (string) {
+  string = string || ''
+  var parsed = compile(string)
 
   /**
    * Check if a filter matches all of the given tags.
    * @param tags {String} Tags in the form of "is:myTag not:myOtherTag"
    * @returns {boolean}
    */
-  function match(tags) {
-    var include = (parsed.is.length === 0)  || matchFilter(tags, parsed.is);
-    var exclude = (parsed.not.length !== 0) && matchFilter(tags, parsed.not);
-    return include && !exclude;
+  function match (tags) {
+    var include = parsed.is.length === 0 || matchFilter(tags, parsed.is)
+    var exclude = parsed.not.length !== 0 && matchFilter(tags, parsed.not)
+    return include && !exclude
   }
 
-  function toString() {
-    return string.trim();
+  function toString () {
+    return string.trim()
   }
 
-  function add(part) {
-    string = string + ' ' + part;
-    parsed = compile(string);
+  function add (part) {
+    string = string + ' ' + part
+    parsed = compile(string)
   }
 
-  function remove(part) {
-    string = string.replace(part, '');
-    parsed = compile(string);
+  function remove (part) {
+    string = string.replace(part, '')
+    parsed = compile(string)
   }
 
   return {
@@ -33,29 +32,33 @@ module.exports = function Filter(string) {
     add: add,
     remove: remove,
     toString: toString
-  };
-
-};
-
-function compile(string) {
-  return {
-    is:  extract(string, /is:([^\s]+)/g),
-    not: extract(string, /not:([^\s]+)/g)
-  };
-}
-
-function extract(string, regex) {
-  var list = [], match = null;
-  while (match = regex.exec(string)) {
-    list.push(match[1].split('+'));
   }
-  return list;
 }
 
-function matchFilter(tagList, toMatch) {
-  return (toMatch.filter(function (tags) {
-    return (tags.filter(function (tag) {
-      return tagList.includes(tag);
-    }).length > 0);
-  }).length > 0);
+function compile (string) {
+  return {
+    is: extract(string, /is:([^\s]+)/g),
+    not: extract(string, /not:([^\s]+)/g)
+  }
+}
+
+function extract (string, regex) {
+  var list = [],
+    match = null
+  while ((match = regex.exec(string))) {
+    list.push(match[1].split('+'))
+  }
+  return list
+}
+
+function matchFilter (tagList, toMatch) {
+  return (
+    toMatch.filter(function (tags) {
+      return (
+        tags.filter(function (tag) {
+          return tagList.includes(tag)
+        }).length > 0
+      )
+    }).length > 0
+  )
 }
--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
-var argv    = require('minimist')(process.argv.slice(2));
-var Filter  = require('./filter');
+var argv = require('minimist')(process.argv.slice(2))
+var Filter = require('./filter')
 
 /**
  * @param tags {String...} List of tags to match
  */
-function tags() {
-  var chain = {};
-  var either = proxy(tags.filter, arguments);
+function tags () {
+  var chain = {}
+
+  var either = proxy(tags.filter, arguments)
   // it & describe
-  chain.it = either(tags.global.it, tags.global.xit);
-  chain.it.only = tags.global.it.only;
-  chain.it.skip = tags.global.it.skip;
-  chain.xit = tags.global.xit;
-  chain.describe = either(tags.global.describe, tags.global.xdescribe);
-  chain.describe.only = tags.global.describe.only;
-  chain.describe.skip = tags.global.describe.skip;
-  chain.xdescribe = tags.global.xdescribe;
+  chain.it = either(tags.global.it, tags.global.xit)
+  chain.it.only = tags.global.it.only
+  chain.it.skip = tags.global.it.skip
+  chain.xit = tags.global.xit
+  chain.describe = either(tags.global.describe, tags.global.xdescribe)
+  chain.describe.only = tags.global.describe.only
+  chain.describe.skip = tags.global.describe.skip
+  chain.xdescribe = tags.global.xdescribe
   // custom hooks
   if (typeof tags.hooks === 'function') {
-    tags.hooks.call(chain, either);
+    tags.hooks.call(chain, either)
   }
-  return chain;
+  return chain
 }
 
 /**
@@ -30,17 +31,17 @@ function tags() {
  * @returns {function(T match, T skip): T}
  * @template T
  */
-function proxy(filter, tagsToMatch) {
-  var run = filter.match(Array.from(tagsToMatch));
-  return function(match, skip) {
-    return run ? match : skip;
-  };
+function proxy (filter, tagsToMatch) {
+  var run = filter.match(Array.from(tagsToMatch))
+  return function (match, skip) {
+    return run ? match : skip
+  }
 }
 
-tags.Filter = Filter;
-tags.filter = new Filter(argv.tags);
-tags.global  = global;
-tags.proxy = proxy;
-tags.hooks   = null;
+tags.Filter = Filter
+tags.filter = new Filter(argv.tags)
+tags.global = global
+tags.proxy = proxy
+tags.hooks = null
 
-module.exports = tags;
+module.exports = tags
